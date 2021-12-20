3e995554b583dee71775f0661f3d1f82fe46aa39
Roman Hyrych <roman.girich@gmail.com>
2021-12-20 02:58:30 +0200

    3 tasks done

--- js/task-02.js
+++ js/task-02.js
@@ -6,31 +6,12 @@ const ingredients = [
 	'Herbs',
 	'Condiments',
 ];
-const ingredientsList = document.querySelector('#ingredients');
-const ingredientsFirstItem = document.createElement('li');
-ingredientsFirstItem.textContent = ingredients[0];
-ingredientsFirstItem.classList = 'item';
-const ingredientsSecondItem = document.createElement('li');
-ingredientsSecondItem.textContent = ingredients[1];
-ingredientsSecondItem.classList = 'item';
-const ingredientsThirdItem = document.createElement('li');
-ingredientsThirdItem.textContent = ingredients[2];
-ingredientsThirdItem.classList = 'item';
-const ingredientsFourthItem = document.createElement('li');
-ingredientsFourthItem.textContent = ingredients[3];
-ingredientsFourthItem.classList = 'item';
-const ingredientsFifthItem = document.createElement('li');
-ingredientsFifthItem.textContent = ingredients[4];
-ingredientsFifthItem.classList = 'item';
-const ingredientsSixthItem = document.createElement('li');
-ingredientsSixthItem.textContent = ingredients[5];
-ingredientsSixthItem.classList = 'item';
 
-ingredientsList.append(
-	ingredientsFirstItem,
-	ingredientsSecondItem,
-	ingredientsThirdItem,
-	ingredientsFourthItem,
-	ingredientsFifthItem,
-	ingredientsSixthItem,
-);
+const ingredientsList = document.querySelector('#ingredients');
+const elements = ingredients.map(ingredient => {
+	const ingredientsItem = document.createElement('li');
+	ingredientsItem.textContent = ingredient;
+	ingredientsItem.classList = 'item';
+	return ingredientsItem;
+});
+ingredientsList.append(...elements);
