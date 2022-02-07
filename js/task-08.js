78507aa0323f0e766b4a8dfe40e3dc9b9b17c845
Roman Hyrych <roman.girich@gmail.com>
2022-02-07 02:20:49 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,13 +2,16 @@ const form = document.querySelector('.login-form');
 
 form.addEventListener('submit', event => {
 	event.preventDefault();
-
+	if (
+		form.children[0].firstElementChild.value === '' ||
+		form.children[1].firstElementChild.value === ''
+	) {
+		alert('no empty fields allowed');
+		return;
+	}
 	const formData = new FormData(event.currentTarget);
 
 	formData.forEach((value, name) => {
-		if (value === '') {
-			alert('no empty fields allowed');
-		}
 		console.log('name:', name);
 		console.log('value:', value);
 	});
